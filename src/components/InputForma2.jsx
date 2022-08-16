d90838f2286f3afef34180d34c76047aada8ae6f
Bojan Golic <bokigolic32@gmail.com>
2022-08-16 01:08:37 -0400

    add input button in form, input works, clear works

--- src/components/InputForma2.jsx
+++ src/components/InputForma2.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
-const InputForma2 = ()=>{
+const InputForma2 = () => {
 
-  const preset= {
-    message:""
+  const preset = {
+    message: ""
   }
-  const newYear= {
-    message:"Happy New Year!!!"
+  const newYear = {
+    message: "Happy New Year!!!"
   }
-  const happyBday= {
-    message:"Happy birthday to you!!!"
+  const happyBday = {
+    message: "Happy birthday to you!!!"
   }
 
 
@@ -23,11 +23,13 @@ const InputForma2 = ()=>{
       <h1>Form</h1>
       <label>Message</label>
       <input
-      type='text'
-      name='message'
-      value={state.message}
+        type='text'
+        name='message'
+        value={state.message}
       />
-      <button type="button" onClick={()=>{setState(preset)}}>Clear</button>
+      <button type="button" onClick={() => { setState(preset) }}>Clear</button>
+      <button type="button" onClick={() => { setState(newYear) }}>Happy NY</button>
+      <button type="button" onClick={() => { setState(happyBday) }}>Happy B-day</button>
     </div>
   );
 };
